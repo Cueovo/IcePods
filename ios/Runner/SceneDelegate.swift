fb8b14444841fd01c568403762a82c44a8e73e50
Cueovo <ilikeuallthetime@gmail.com>
2026-07-24 01:56:00 +0800

    Use iOS 16 MPNowPlayingSession for Dynamic Island app launch

--- ios/Runner/SceneDelegate.swift
+++ ios/Runner/SceneDelegate.swift
@@ -2,5 +2,19 @@ import Flutter
 import UIKit
 
 class SceneDelegate: FlutterSceneDelegate {
+  override func sceneWillEnterForeground(_ scene: UIScene) {
+    super.sceneWillEnterForeground(scene)
+    guard let windowScene = scene as? UIWindowScene else { return }
+    for window in windowScene.windows {
+      window.makeKeyAndVisible()
+    }
+  }
 
+  override func sceneDidBecomeActive(_ scene: UIScene) {
+    super.sceneDidBecomeActive(scene)
+    guard let windowScene = scene as? UIWindowScene else { return }
+    for window in windowScene.windows {
+      window.makeKeyAndVisible()
+    }
+  }
 }
